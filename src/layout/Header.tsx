5e098d82bf7e07ab6107e01e6a8423d7d4ddf3e1
Carlos Rangel <carlos.developer17@gmail.com>
2021-08-18 23:22:26 -0500

    Added mobile menu icon

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,12 @@
-import React, { FunctionComponent, useContext } from 'react'
+import React, { FunctionComponent, useContext, useState } from 'react'
 import Logo from '~svg/Logo'
 import DarkToggle from '~components/DarkToggle'
 import { ThemeContext } from '~contexts/theme'
 import clsx from 'clsx'
 
 import styles from './header.module.scss'
+import { useWindowDimensions } from '~contexts/window-dimensions'
+import Menu from '~svg/Manu'
 
 type Props = {
   homepage?: string
@@ -12,6 +14,26 @@ type Props = {
 
 const Header: FunctionComponent<Props> = () => {
   const { colorMode } = useContext(ThemeContext)
+  const { isMobile } = useWindowDimensions()
+  const [toggleDrawer, setToggleDrawer] = useState(false)
+
+  const handleToggle = () => {
+    setToggleDrawer(!toggleDrawer)
+  }
+  const menu = isMobile ? (
+    <button className={styles.menu} type="button" onClick={handleToggle}>
+      <Menu />
+    </button>
+  ) : (
+    <ul className={styles.list}>
+      <li>
+        <a href="#Home">Home</a>
+      </li>
+      <li>
+        <a href="#Education">Education</a>
+      </li>
+    </ul>
+  )
 
   return (
     <header className={clsx(styles.header, colorMode === 'dark' && styles.secondary)}>
@@ -20,16 +42,7 @@ const Header: FunctionComponent<Props> = () => {
           <Logo className={styles.logo} fillColor="#fff" />
         </a>
         <DarkToggle />
-        <div className={styles.listSection}>
-          <ul className={styles.list}>
-            <li>
-              <a href="#Home">Home</a>
-            </li>
-            <li>
-              <a href="#Education">Education</a>
-            </li>
-          </ul>
-        </div>
+        <div className={styles.listSection}>{menu}</div>
       </nav>
     </header>
   )
