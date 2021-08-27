d35bb3a6030aaa004f0bf85f6b902edcfb1dc6f1
Carlos Rangel <carlos.developer17@gmail.com>
2021-08-26 23:18:19 -0500

    Added logos (WIP)

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,20 +4,61 @@ import SectionLayout from '~layout/SectionLayout'
 import styles from './skills.module.scss'
 import JavaScript from '~svg/JavaScript'
 import CSharp from '~svg/CSharp'
-import ReactJS from './React'
+import ReactJS from '../../svg/React'
+import Angular from '~svg/Angular'
+import React from 'react'
+import HTML from '~svg/HTML'
+import Sass from '~svg/Sass'
+import Jest from '~svg/Jest'
+import NodeJS from '~svg/NodeJS'
+import CSS from '~svg/CSS'
+import JQuery from '~svg/jQuery'
+import SQLServer from '~svg/SQLServer'
+import TypeScript from '~svg/TypeScript'
+import Figma from '~svg/Figma'
+import NETCore from '~svg/NETCore'
+import NextJS from '~svg/NextJS'
+import Git from '~svg/Git'
 
 interface Props {
   [rest: string]: unknown
 }
 
-const Skills: FunctionComponent<Props> = ({ ...rest }) => (
-  <SectionLayout title="Skills" {...rest}>
-    <div className={styles.container}>
-      <JavaScript />
-      <CSharp />
-      <ReactJS />
-    </div>
-  </SectionLayout>
-)
+const Skills: FunctionComponent<Props> = ({ ...rest }) => {
+  // rgba(0, 0, 0, 0.035)
+  const skillLogos = [
+    <ReactJS />,
+    <Angular />,
+    <CSharp />,
+    <NodeJS />,
+    <TypeScript />,
+    <HTML />,
+    <CSS />,
+    <Sass />,
+    <JavaScript />,
+    <JQuery />,
+    <Jest />,
+    <SQLServer />,
+    <Figma />,
+    <NETCore />,
+    <NextJS />,
+    <Git />
+  ]
+  const formatSkills = skillLogos.map((logo: JSX.Element, n: number) =>
+    React.cloneElement(logo, {
+      key: skillLogos.indexOf(logo),
+      height: '150px',
+      style: {
+        background: `rgba(200, 200, 200, ${0.035 * ((n - ()) + 1)})`
+      },
+      width: '150px'
+    })
+  )
 
+  return (
+    <SectionLayout title="Skills" {...rest}>
+      <div className={styles.container}>{formatSkills}</div>
+    </SectionLayout>
+  )
+}
 export default Skills
