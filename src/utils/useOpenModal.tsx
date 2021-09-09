91f122e7d58afacead861f00a6fa52f8340bdfec
Carlos Rangel <carlos.developer17@gmail.com>
2021-09-08 23:59:54 -0500

    Removed isSafari usage

--- src/utils/useOpenModal.tsx
+++ src/utils/useOpenModal.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
-
-import isClient from './isClient'
+import { useEffect, useRef } from 'react'
 
 type Props = {
   isOpen: boolean
@@ -9,7 +7,6 @@ type Props = {
 }
 
 const useOpenModal = ({ isOpen, setIsShowing, closeBtnRef }: Props) => {
-  const [isSafari] = useState(() => (isClient ? /^((?!chrome|android).)*safari/i.test(navigator.userAgent) : false))
   const firstUpdate = useRef(true)
 
   useEffect(() => {
@@ -22,12 +19,6 @@ const useOpenModal = ({ isOpen, setIsShowing, closeBtnRef }: Props) => {
       document.body.style.overflow = 'hidden'
       document.documentElement.style.overflow = 'hidden'
 
-      // safari doesn't respect overflows on body/html. You need to set the position to fixed
-      if (isSafari) {
-        // document.body.style.top = `${-window.pageYOffset}px`
-        // document.body.style.position = 'fixed'
-      }
-
       if (closeBtnRef.current !== null) {
         closeBtnRef.current.focus()
       }
@@ -35,22 +26,10 @@ const useOpenModal = ({ isOpen, setIsShowing, closeBtnRef }: Props) => {
       setIsShowing(false)
       document.body.style.overflow = ''
       document.documentElement.style.overflow = ''
-      // with a fixed position, the scroll goes to the top.
-      // After setting the top, we grab that value and scroll to it to restore scroll position
-      // if (isSafari) {
-      //   const offsetY = Math.abs(parseInt(document.body.style.top || '0', 10))
-      //   document.body.style.position = ''
-      //   document.body.style.top = ''
-      //   window.scrollTo(0, offsetY || 0)
-      // }
     }
     return () => {
       document.body.style.overflow = ''
       document.documentElement.style.overflow = ''
-      if (isSafari) {
-        document.body.style.position = ''
-        document.body.style.top = ''
-      }
     }
   }, [isOpen])
 }
